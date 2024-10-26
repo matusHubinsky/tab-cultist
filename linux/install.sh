59ad7142c73b11f8ca578e16bd77bf9fa52e6e23
Matus Hubinsky <matus.hubinsky@proton.me>
2024-10-26 15:05:08 +0200

    initial flex screen

--- linux/install.sh
+++ linux/install.sh
@@ -5,8 +5,6 @@ cat logo.txt
 echo "From the dephts of hell I raise"
 echo "Installing tab-cultist"
 
-cat logo.txt
-
 echo "Checking root priviliges ..."
 if [ $EUID != 0 ]; then
     sudo bash "$0" "$@"
@@ -21,16 +19,15 @@ cp ./tab-cultist /usr/bin/
 echo "Creating directory 'tab-cultist'"
 mkdir /usr/share/tab-cultist
 mkdir /usr/share/tab-cultist/font
-mkdir /usr/share/tab-cultist/font/Roboto_Mono
 
 echo "Moving tab-cultist database and modules to /usr/share/tab-cultist"
 cp linux/./icon.png /usr/share/tab-cultist/
 cp linux/./tab-cultist.desktop /usr/share/tab-cultist/
 cp ./config.txt  /usr/share/tab-cultist/
-cp font/ -r /usr/share/tab-cultist/font/Roboto_Mono/
+cp font/ -r /usr/share/tab-cultist/
 cp database/ -r /usr/share/tab-cultist/
 cp ./logo.txt /usr/share/tab-cultist/
 
 cp config.txt /usr/share/tab-cultist/config.txt
 
-echo "Done. Have fun."
+echo "Installation is complete. Have fun."
